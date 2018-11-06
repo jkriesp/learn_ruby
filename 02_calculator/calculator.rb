f164ea778259c11b655410586bf81061e0728727
Jens Kristian Espevik <jenskristian@gmail.com>
2018-11-06 09:56:45 +0100

    finish calculator.rb testing

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add(a, b)
+    return a + b
+end
+
+def subtract(a, b)
+    return a - b
+end
+
+def sum(array)
+    sum = 0
+    array.each do |i|
+        sum = sum + i
+    end
+    return sum
+end
+
+def multiply(a, b)
+    return a * b
+end

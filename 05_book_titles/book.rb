5bb16d78bbbc38ac005af0879ec45d35a7ab090e
Jens Kristian Espevik <jenskristian@gmail.com>
2018-11-07 20:53:05 +0100

    finish all but the last test

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+
+    attr_writer :title
+
+    def title
+        small_words = ["the" , "a", "an", "and", "in", "of"]
+        if @title.strip.include? " "
+            title = @title.split.map { |x| small_words.include?(x) || x.to_i != 0 ? x : x.capitalize }.join(" ")                       
+        else
+            title = @title.capitalize          
+        end             
+        return title
+    end
+
 end
